a67c8caec7702c2521b0b0e6f3eba02cb17f9d3c
hschack <hschack@gmail.com>
2023-04-27 23:09:46 +0200

    270423_2308

--- src/main.cpp
+++ src/main.cpp
@@ -13,14 +13,16 @@ Description:
 #include <Arduino.h>
 #include <Wire.h>
 #include "AsyncSMS.h"
+#include <Adafruit_INA219.h>
 /***************************   Defines   *************************************/
 /***************************   Flags and bit *********************************/
-//bool sendAlarmSMS           =  true;
+bool NewSMS           =  false;
 /***************************   sbit ******************************************/
 /***************************   Macros   **************************************/
 /***************************   Data Types   **********************************/
 /***************************   Local Variables   *****************************/
 u_int16_t SMScount = 1;
+u_int8_t dataPCFread       = 0;
 String msg;
 /*****************************************************************************/
 /***************************   Enum   ****************************************/
@@ -38,8 +40,8 @@ AsyncSMS smsHelper( &Serial1, 19200 );
 void setup(void);
 void SetupSTM32(void);
 void loop(void);
-u_int8_t ReadPCF8574();
-void TestIndput(u_int8_t dataPCFread);
+void ReadPCF8574(void);
+void TestIndput(void);
 void SendSMStxt(void);
 void TestInputAndSendSMS(void);
 void messageReceived(char * number, char * message);
@@ -49,11 +51,12 @@ Function name : void setup()
 Description   : Run after start/reset
 Notes :
 ******************************************************************************/
-void setup() {
+void setup() 
+     {
       SetupSTM32();
       smsHelper.init();
       smsHelper.smsReceived = *messageReceived;
-} // END SETUP
+     } // END SETUP
 /******************************************************************************
 Function name : void Loop()
          Type : PRIVATE
@@ -61,9 +64,11 @@ Description   :
 Notes :
 ******************************************************************************/
 /***************************   Local Variables   *****************************/
-void loop(){
+void loop()
+ {
    smsHelper.process();  // call in main loop
    TestInputAndSendSMS();
+//   delay(500);
  }   // END loop 
 /******************************************************************************
 Function name : void SetupStm32(void)
@@ -71,11 +76,13 @@ Function name : void SetupStm32(void)
 Description   : 
 Notes :
 ******************************************************************************/
-void SetupSTM32(void){
+void SetupSTM32(void)
+  {
     Serial2.begin( 115200 );
     while ( !Serial2 );
     Wire1.begin();
     pinMode( PC13, OUTPUT );    // sets the digital pin 13 as output
+    analogReadResolution(12);
     delay( 100 );
     Serial2.println( "Start UP" );
   } // END SETUP
@@ -90,26 +97,27 @@ Function name : void SendSMStxt(void)
 Description   : 
 Notes :
 ******************************************************************************/
-void SendSMStxt(void){
+void SendSMStxt(void)
 /***************************   Local Variables   *****************************/
+  {
      SMScount++;
      digitalWrite(PC13 , LOW);
-     smsHelper.send("+4522360040", (char *)msg.c_str(), msg.length());
+     //smsHelper.send("+4522360040", (char *)msg.c_str(), msg.length());
      Serial2.write(msg.c_str(), msg.length());
      digitalWrite(PC13 , HIGH);
-} // end 
+  } // end 
 /******************************************************************************
 Function name : u_int8_t ReadPCF8574(void) 
          Type :
 Description   : 
 Notes :
 ******************************************************************************/
-u_int8_t ReadPCF8574(void){ 
-   u_int8_t x; 
+void ReadPCF8574(void){ 
+   //u_int8_t x; 
      Wire1.beginTransmission( 0x20 );
      Wire1.endTransmission();
      Wire1.requestFrom( 0x20 , 1 );
-     return x;
+     //return x;
 }
 /******************************************************************************
 Function name : void messageReceived(char * number, char * message)
@@ -117,72 +125,87 @@ Function name : void messageReceived(char * number, char * message)
 Description   : 
 Notes :
 *******************************************************************************/
-void messageReceived(char * number, char * message) {
+void messageReceived(char * number, char * message) 
+  {
   //Do something with your message
   Serial2.println("Message received");
   Serial2.println(number);
   Serial2.println(message);
-}
+  }
 /******************************************************************************
-Function name : void TestIndput(void) 
+Function name : void TestIndput(u_int8_t dataPCFread) 
          Type :
 Description   : 
 Notes :
 ******************************************************************************/
- void TestIndput(u_int8_t dataPCFread){ 
-    msg = "Samsoe 4 x BWE 632\n";
-    msg += "Zone 1: ";  //Zone 1
-     if (bitRead( dataPCFread, 0 ) != true){ // test Zone 1 for low pellets
+void TestIndput(void)
+ { 
+     msg = "Samsoe 4 x BWE 632\n";
+     msg += "Zone 1: ";  //Zone 1
+     if (bitRead( dataPCFread, 0 ) != true) // test Zone 1 for low pellets
+     {
         msg += "LAV\n";
      }
-    else{
-		    msg += "OK\n";
-	   }
+     else
+     {
+		  msg += "OK\n";
+	  }
     msg += "Zone 2: ";  //Zone 2 
-     if (bitRead( dataPCFread, 1 ) != true){ // test Zone 2 for low pellets
+     if (bitRead( dataPCFread, 1 ) != true) // test Zone 2 for low pellets
+     {
         msg += "LAV\n";
      }
-    else{
-		    msg += "OK\n";
-	   }
+     else
+     {
+		  msg += "OK\n";
+	  }
     msg += "Zone 3: ";  //Zone 3
-     if (bitRead( dataPCFread, 2 ) != true){ // test Zone 3 for low pellets
+     if (bitRead( dataPCFread, 2 ) != true) // test Zone 3 for low pellets
+     {
         msg += "LAV\n";
      }
-    else{
-		    msg += "OK\n";
+     else
+     {
+		  msg += "OK\n";
 	   }
     msg += "Zone 4: ";  //Zone 4
-     if (bitRead( dataPCFread, 3 ) != true){ // test Zone 4 for low pellets
+     if (bitRead( dataPCFread, 3 ) != true) // test Zone 4 for low pellets
+     {
         msg += "LAV\n";
      }
-    else{
+     else
+     {
 		    msg += "OK\n";
-	   }
+	  }
     msg += "Fase 1: ";  //Fase 1
-     if (bitRead( dataPCFread, 4 ) != true){ // test Fase 1 for power
+     if (bitRead( dataPCFread, 4 ) != true) // test Fase 1 for power
+     {
         msg += "OFF\n";
      }
-    else{
-		    msg += "OK\n";
-	   }
+     else
+     {
+		  msg += "OK\n";
+	  }
     msg += "Fase 2: ";  //Fase 2   
-     if (bitRead( dataPCFread, 5 ) != true){ // test Fase 2 for power
+     if (bitRead( dataPCFread, 5 ) != true) // test Fase 2 for power
+     {
         msg += "OFF\n";
      }
     else{
 		    msg += "OK\n";
 	   }
     msg += "Fase 3: ";  //Fase 3  
-     if (bitRead( dataPCFread, 6 ) != true){ // test Fase 3 for power
+     if (bitRead( dataPCFread, 6 ) != true) // test Fase 3 for power
+     {
         msg += "OFF\n";
      }
-    else{
-		    msg += "OK\n";
-	   }
-          msg += "SMS No: ";
-          msg += SMScount;
-          msg += "\n";
+     else
+     {
+		  msg += "OK\n";
+	  }
+        msg += "SMS No: ";
+        msg += SMScount;
+        msg += "\n";
 } // end check input
 /******************************************************************************
 Function name : void TestInputAndSendSMS(void) 
@@ -190,22 +213,33 @@ Function name : void TestInputAndSendSMS(void)
 Description   : 
 Notes :
 ******************************************************************************/
-void TestInputAndSendSMS(void){
+void TestInputAndSendSMS(void)
+{
 /***************************   Local Variables   *****************************/
-static u_int32_t ReadPCFinterval = 0;
-u_int32_t mili_time = 0;
-static u_int8_t Readcount = 0; 
-static u_int8_t indputAndSmsState = 0;
-static u_int8_t dataPCFread       = 0;
-static u_int8_t dataPCFold        = 0;
-static bool WaitForData = false;
+static unsigned long ReadPCFinterval = 0;
+unsigned long mili_time = 0;
+static u_int8_t dataPCFold = 0;
+static int Readcount = 0; 
+static int indputAndSmsState = 0;
 
-
-   if(WaitForData)
-   {
+   switch (indputAndSmsState) {
+      case 0: 
+         mili_time = millis();
+         if( mili_time - ReadPCFinterval >= 1000 )
+         { 
+            // Read interval 1000 = 1 sek.
+            ReadPCF8574();
+            ReadPCFinterval = mili_time;
+            Serial2.print(dataPCFread);
+            Serial2.print(" - ");
+            Serial2.println(Readcount);            
+            // Move to next state
+            indputAndSmsState = 1;
+         }
+         break; 
+      case 1: 
          if ( Wire1.available() )
          { 
-            WaitForData = false;
             // if 1 byte received
             // Read byte from i2c PC8574  
             dataPCFread = Wire1.read();
@@ -216,27 +250,19 @@ static bool WaitForData = false;
             }
             else if(Readcount < 5)
             {
-               Readcount++; 
-               if(Readcount == 5)
-               {
-                  TestIndput(dataPCFread);
-                  SendSMStxt();
-               }
+                Readcount++; 
+                if(Readcount == 5)
+                {
+                    Serial2.println(dataPCFread);
+                }
             }
-        }
-   }
-   else // WaitForData = FALSE
-   {
-       mili_time = millis();
-       if( mili_time - ReadPCFinterval >= 1000 )
-       { 
-        // Read interval 1000 = 1 sek.
-        dataPCFread = ReadPCF8574();
-        ReadPCFinterval = mili_time;
-        WaitForData = true;
-      }
-   }
-}   
+           indputAndSmsState = 0;
+         }
+         break;
+      default:
+         break;
+   }      
+}    
 /******************************************************************************
 Function name : void 
          Type : 
